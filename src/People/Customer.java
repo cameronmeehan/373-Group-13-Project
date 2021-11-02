9c319059b4a7210444c9eb24fff2352dffdef6fc
cameronmeehan <70229852+cameronmeehan@users.noreply.github.com>
2021-11-01 18:22:11 -0700

    setting up project
    setting up project

---
+++ src/People/Customer.java
@@ -0,0 +1,19 @@
+package People;
+
+public class Customer extends Person {
+
+	private int creditCardNumber;
+	
+	public void goToOnlineCheckout() {
+		
+		//brings customer to online checkout page?
+	}
+
+	public int getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCardNumber(int creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+}
--- src/People/Person.java
+++ src/People/Person.java
@@ -2,4 +2,28 @@ package People;
 
 public class Person {
 
+	protected String name;
+	protected String adress;
+	protected int phoneNumber;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 }
