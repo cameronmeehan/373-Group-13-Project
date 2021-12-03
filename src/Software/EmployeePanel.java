39123e14c2bc2785d824bd4b284c358598f43d03
HeeroZero <74216391+HeeroZero@users.noreply.github.com>
2021-12-03 00:18:00 -0700

    Updated Employee Panel

--- src/Software/EmployeePanel.java
+++ src/Software/EmployeePanel.java
@@ -22,17 +22,19 @@ public class EmployeePanel extends JPanel implements ActionListener{
 	// inventory stuff
 	private JLabel stockFunction;
 	private JLabel InventoryBanner;
-	private JTextField iIncreaseStock;
-	private JTextField iRemoveStock;
-	private JButton stockIncrease;
-	private JButton stockRemove;
+	//private JTextField iIncreaseStock;
+	//private JTextField iRemoveStock;
+	//private JButton stockIncrease;
+	//private JButton stockRemove;
 	private JScrollPane InventoryScroll;
+	private JLabel checkQuantity;
+	private JTextField checkInventory;
+	private JButton quantityButton;
+	private JButton changeStock;
+	private JLabel quantity;
 	
-	// employment years
-	private JLabel YearBanner;
-	private JTextField setYears;
-	private JButton changeYears;
-	private String years = "0";
+	// Stock pop up
+	private JDialog stockPopUp;
 	
 	// in-person checkout
 	private JButton inPersonCheckout;
@@ -49,32 +51,34 @@ public class EmployeePanel extends JPanel implements ActionListener{
 		welcomeBanner.setFont(new Font("Calibri", Font.BOLD, 45));
 		add(welcomeBanner);
 		
-		YearBanner = new JLabel("Employed for : " + years + " years");
-		YearBanner.setFont(new Font("Calibri", Font.BOLD, 40));
-		setYears = new JTextField("Years");
-		changeYears = new JButton("Change years");
-		add(YearBanner); add(setYears); add(changeYears);
-		
 		InventoryBanner = new JLabel("List of Inventory:");
 		InventoryBanner.setFont(new Font("Calibri", Font.BOLD, 40));
 		InventoryScroll = new JScrollPane(ListFromArrayInventory(GroceryStore.InventoryList));
 		stockFunction = new JLabel("Stock Inventory");
 		stockFunction.setFont(new Font("Calibri", Font.BOLD, 40));
-		iIncreaseStock = new JTextField("Quantity");
-		iRemoveStock = new JTextField("Quantity");
-		add(InventoryBanner); add(InventoryScroll); add(stockFunction); add(iIncreaseStock); add(iRemoveStock);
+		checkQuantity = new JLabel("Please enter an invetory item to check or change stock:");
+		checkInventory = new JTextField("Inventory");
+		quantityButton = new JButton("Check Stock");
+		changeStock = new JButton("Change Stock");
+		quantityButton.addActionListener(this);
+		changeStock.addActionListener(this);
+		quantity = new JLabel();
 		
+		add(InventoryBanner); add(InventoryScroll); add(stockFunction);
+		add(checkQuantity); add(checkInventory); add(quantityButton); add(changeStock); 
+		add(quantity);
+
 		//general page stuff alignment
 		exitButton.setBounds(850,600,100,100);
 		welcomeBanner.setBounds(200, 50, 500, 50);
-		YearBanner.setBounds(200, 110, 500, 50);
-		setYears.setBounds(200, 150, 250, 35);
-		changeYears.setBounds(600, 150, 250, 35);
 		InventoryBanner.setBounds(200, 200, 350, 40);
 		InventoryScroll.setBounds(200, 245, 250, 75);
 		stockFunction.setBounds(200, 350, 350, 50);
-		iIncreaseStock.setBounds(200, 405, 250, 50);
-		iRemoveStock.setBounds(200, 460, 250, 50);
+		checkQuantity.setBounds(200, 405, 300, 50);
+		checkInventory.setBounds(200, 460, 250, 50);
+		quantityButton.setBounds(455, 460, 200, 50);
+		changeStock.setBounds(660, 460, 200, 50);
+		quantity.setBounds(200, 515, 250, 50);
 		
 		
 		//adding stuff to page
@@ -85,12 +89,31 @@ public class EmployeePanel extends JPanel implements ActionListener{
 			top.runLogin();
 		}
 		
-		// Needs fixing
-		if (e.getSource() == changeYears) {
-			years = setYears.getText();
-			remove(YearBanner);
-			add(YearBanner);
+		if (e.getSource() == quantityButton) {
+			for(int i = 0; i < GroceryStore.InventoryList.size(); i ++) {
+				if(GroceryStore.InventoryList.get(i).getName().equals(checkInventory.getText())){
+
+					quantity.setText(GroceryStore.InventoryList.get(i).getName() + ": "
+							+ GroceryStore.InventoryList.get(i).getStock());
+					
+					remove(quantity);
+					add(quantity);
+				}
+			}
 		}
+		
+		if (e.getSource() == changeStock) {
+			stockPopUpWindow();
+		}
+		
+		//if (e.getSource() == iIncreaseStock) {
+			//for(int i = 0; i < GroceryStore.InventoryList.size(); i++) {
+				//if(GroceryStore.InventoryList.get(i).getName().equals(checkInventory.getText())) {
+					
+				//}
+			//}
+		//}
+		
 				
 	}
 	
@@ -103,6 +126,33 @@ public class EmployeePanel extends JPanel implements ActionListener{
 		
 		JList lTemp = new JList(temp);
 		return(lTemp);
+	}
+	
+	public void stockPopUpWindow() {
+		stockPopUp = new JDialog();
+		stockPopUp.setTitle(checkInventory.getText() + " Stock");
+		stockPopUp.setSize(700,700);
+		stockPopUp.setLayout(null);
+		stockPopUp.setVisible(true);
+		
+		JLabel stockBanner = new JLabel("Please enter an amount to increase or remove stock");
+		stockBanner.setFont(new Font("Calibri", Font.BOLD, 20));
+		stockBanner.setBounds(100, 100, 450, 50);
+		stockPopUp.add(stockBanner);
+		
+		JTextField amount = new JTextField("Amount");
+		amount.setBounds(100, 155, 200, 50);
+		stockPopUp.add(amount);
+		
+		JButton iIncreaseStock = new JButton("Increase");
+		JButton iRemoveStock = new JButton("Remove");
+		iIncreaseStock.setBounds(305, 155, 150, 50);
+		iRemoveStock.setBounds(460, 155, 150, 50);
+		iIncreaseStock.addActionListener(this);
+		iRemoveStock.addActionListener(this);
+		stockPopUp.add(iIncreaseStock);
+		stockPopUp.add(iRemoveStock);
+		
 	}
 	
 }
